069fc46967b52743e960c7cabdf5710d29fb0748
nickzylstra <nick.zylstra@gmail.com>
2019-12-19 12:34:37 -0800

    Remove caps from schema

--- database/schema.sql
+++ database/schema.sql
@@ -1,39 +1,39 @@
 CREATE TABLE stocks (
-  stockId SERIAL PRIMARY KEY,
-  stockName VARCHAR(50),
+  stock_id SERIAL PRIMARY KEY,
+  stock_name VARCHAR(50),
   symbol CHAR(5),
-  analystHold SMALLINT
+  analyst_hold SMALLINT
 );
 
 CREATE TABLE tags (
-  tagId SERIAL PRIMARY KEY,
-  tagName VARCHAR(50)
+  tag_id SERIAL PRIMARY KEY,
+  tag_name VARCHAR(50)
 );
 
 CREATE TABLE stockTags (
-  stockTagId SERIAL PRIMARY KEY,
-  tagId INT REFERENCES tags,
-  stockId INT REFERENCES stocks
+  stock_tag_id SERIAL PRIMARY KEY,
+  tag_id INT REFERENCES tags,
+  stock_id INT REFERENCES stocks
 );
 
 CREATE TABLE prices (
-  priceId SERIAL PRIMARY KEY,
-  stockId INT REFERENCES stocks,
+  price_id SERIAL PRIMARY KEY,
+  stock_id INT REFERENCES stocks,
   ts TIMESTAMPTZ,
   -- break out timestamp into subcomponents for faster querying?
   price NUMERIC(10,2)
 );
 
 CREATE TABLE users (
-  userId SERIAL PRIMARY KEY,
-  firstName VARCHAR(35),
-  lastName VARCHAR(35),
+  user_id SERIAL PRIMARY KEY,
+  firstname VARCHAR(35),
+  lastname VARCHAR(35),
   balance NUMERIC(12,2)
 )
 
 CREATE TABLE userStocks (
-  userStockId SERIAL PRIMARY KEY,
-  userId INT REFERENCES users,
-  stockId INT REFERENCES stocks,
+  user_stock_id SERIAL PRIMARY KEY,
+  user_id INT REFERENCES users,
+  stock_id INT REFERENCES stocks,
   quantity INT
 )
