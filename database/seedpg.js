2659caa5013c32f5a5ca9e37c6015c1cdf7bcedc
nickzylstra <nick.zylstra@gmail.com>
2019-12-20 14:43:07 -0800

    Refactor varnames

--- database/seedpg.js
+++ database/seedpg.js
@@ -18,48 +18,18 @@ const seedStocks = (dbConn) => {
 };
 
 const seedPrices = async (dbConn) => {
-  // const batchStockCount = 5;
-  // const batchCount = Math.ceil(stocksCount / batchStockCount);
-  // for (let k = 0; k < batchCount; k += 1) {
-  //   const queries = [];
-  //   for (let i = 0, stockIdx = i + k * batchStockCount;
-  //     i < batchStockCount && stockIdx < stocksCount;
-  //     i += 1, stockIdx += 1) {
-  //     const stock = stocks[stockIdx];
-  //     const s = stock[0];
-
-  //     const priceCount = 5 * 8760 * 12;
-  //     const prices = genPriceHistory(priceCount);
-
-  //     let query = 'INSERT INTO prices (stock_symbol, ts, price) VALUES\n';
-  //     for (let j = 0; j < priceCount; j += 1) {
-  //       const price = prices[j];
-  //       const [ts, p] = price;
-  //       query += `('${s}', '${ts}', ${p}),\n`;
-  //     }
-  //     query = `${query.substring(0, query.length - 2)};`;
-
-  //     fancy(`node sent prices db insert for stock ${stockIdx + 1}`);
-  //     queries.push(dbConn.query(query));
-  //   }
-
-  //   // eslint-disable-next-line no-await-in-loop
-  //   await Promise.all(queries);
-  //   fancy(`db completed prices db insert for batch ${k + 1}/${batchCount}`);
-  // }
-
   const queryChains = [];
-  const concurrentCount = 5;
-  const iterCount = Math.ceil(stocksCount / concurrentCount);
-  const chainIters = [];
-  for (let i = 0; i < iterCount; i += 1) {
-    chainIters.push(i);
+  const chainCount = 5;
+  const chainLinkCount = Math.ceil(stocksCount / chainCount);
+  const chainLinks = [];
+  for (let i = 0; i < chainLinkCount; i += 1) {
+    chainLinks.push(i);
   }
 
   const queryChain = (concurrentIdx, iters) => (
     iters.reduce((chain, chainIter) => (
       chain.then(() => {
-        const stockIdx = chainIter + iterCount * concurrentIdx;
+        const stockIdx = chainIter + chainLinkCount * concurrentIdx;
         if (stockIdx >= stocksCount) {
           return Promise.resolve();
         }
@@ -77,14 +47,14 @@ const seedPrices = async (dbConn) => {
         }
         query = `${query.substring(0, query.length - 2)};`;
 
-        fancy(`node sent prices db insert for stock ${stockIdx + 1}/${stocksCount}`);
+        fancy(`initiated db insert for stock ${stockIdx + 1}/${stocksCount} prices`);
         return dbConn.query(query);
       })
     ), Promise.resolve())
   );
 
-  for (let i = 0; i < concurrentCount; i += 1) {
-    queryChains.push(queryChain(i, chainIters));
+  for (let i = 0; i < chainCount; i += 1) {
+    queryChains.push(queryChain(i, chainLinks));
   }
 
   return Promise.all(queryChains);
