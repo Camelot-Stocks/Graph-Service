071d4971ef12a426d1f4e8a793258b8151a941cf
nickzylstra <nick.zylstra@gmail.com>
2019-12-21 15:01:46 -0800

    Setup auth file for db conn

---
+++ database/authcs.js
@@ -0,0 +1,9 @@
+const clientOptions = {
+  contactPoints: ['localhost'],
+  localDataCenter: 'datacenter1',
+  keyspace: 'stock_history',
+};
+
+module.exports = {
+  clientOptions,
+};
--- database/indexcs.js
+++ database/indexcs.js
@@ -1,17 +1,13 @@
 const { Client } = require('cassandra-driver');
 const fancy = require('fancy-log');
+const { clientOptions } = require('./authcs');
 
 const createConn = async () => {
-  const clientOptions = {
-    contactPoints: ['localhost'],
-    localDataCenter: 'datacenter1',
-    keyspace: 'stock_history',
-  };
   const client = new Client(clientOptions);
-  await client.connect();
-  fancy('db connected');
+
+  return client;
 };
 
 module.exports = {
-  dbConn: createConn().catch(fancy),
+  dbClient: createConn().catch(fancy),
 };
