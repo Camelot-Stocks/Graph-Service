e4f6800dc5cd80793cc75ac9defb449610364a02
nickzylstra <nick.zylstra@gmail.com>
2019-12-22 11:40:28 -0800

    Create csv files for prices

--- database/seedcs.js
+++ database/seedcs.js
@@ -2,19 +2,23 @@ const { Client } = require('cassandra-driver');
 const fancy = require('fancy-log');
 const { clientOptions } = require('./authcs');
 const {
-  genStockRow,
   genCSV,
-  // genStocksCSV,
+  genStockRow,
+  genPriceHistoryRows,
 } = require('./seeddatagen');
 
 
 const seed = async () => {
   const client = new Client(clientOptions);
 
-  const stocksCount = 2000000;
-  // eslint-disable-next-line no-unused-vars
-  // const [csvFile, symbols] = await genStocksCSV(stocksCount);
-  await genCSV('stocks.csv', genStockRow, stocksCount);
+  const stocksCount = 20;
+  const stockIds = await genCSV('stocks.csv', genStockRow, stocksCount);
+
+  for (let i = 0; i < stocksCount; i += 1) {
+    const stockId = stockIds[i];
+    // eslint-disable-next-line no-await-in-loop
+    await genCSV(`prices${i}.csv`, genPriceHistoryRows, 1, stockId);
+  }
 
   await client.shutdown();
 };
--- database/seeddatagen.js
+++ database/seeddatagen.js
@@ -4,14 +4,70 @@ const fs = require('fs');
 const path = require('path');
 const fancy = require('fancy-log');
 
+
+const genCSV = async (filename, genRow, rowCount, genRowArgs) => {
+  const csvFile = path.resolve(__dirname, 'seedFiles', filename);
+  const encoding = 'utf-8';
+  const writer = fs.createWriteStream(csvFile);
+
+  const write = (rowCountTotal, rowCountLeft, cb, rowIds = []) => {
+    let canWrite = true;
+    let i = rowCountLeft;
+    const rowBatchCount = 100000;
+    do {
+      const curRowBatchCount = Math.min(i, rowBatchCount);
+      i -= curRowBatchCount;
+      let writeStr = '';
+      for (let j = 0; j < curRowBatchCount; j += 1) {
+        const rowData = genRow(genRowArgs);
+        rowIds.push(rowData[0]);
+        writeStr += rowData[1];
+      }
+
+      fancy(`${rowCountTotal - i}/${rowCountTotal} batches written for ${filename}`);
+
+      if (i === 0) {
+        writer.write(writeStr, encoding, () => cb(rowIds));
+      } else {
+        canWrite = writer.write(writeStr, encoding);
+      }
+    } while (i > 0 && canWrite);
+
+    if (i > 0) {
+      writer.once('drain', () => write(rowCountTotal, i, cb, rowIds));
+    }
+  };
+
+  return new Promise((resolve) => write(rowCount, rowCount, resolve));
+};
+
 const genStockRow = () => {
   const symbol = faker.random.alphaNumeric(5).toUpperCase();
-  const name = faker.company.companyName().replace('\'', '');
+  const name = faker.company.companyName().replace(/'|,/g, '');
   const owners = faker.random.number(20000);
   const analystHold = faker.random.number({ min: 0, max: 100, precision: 1 });
-  // return `'${symbol}', '${name}', ${owners}, ${analystHold}`;
-  // return [symbol, name, owners, analystHold];
-  return [symbol, `'${symbol}', '${name}', ${owners}, ${analystHold}`];
+  return [symbol, `'${symbol}', '${name}', ${owners}, ${analystHold}\n`];
+};
+
+const genPriceHistoryRows = (symbol) => {
+  let price = faker.random.number({ min: 1, max: 1000, precision: 0.01 });
+  let trend = faker.random.number({ min: -1, max: 1, precision: 0.01 });
+  const time = moment('2015-01-01 00:00:00-05');
+
+  const priceCount = 5 * 8760 * 12;
+  let rowsStr = '';
+  for (let i = 0; i < priceCount; i += 1) {
+    const ts = time.format('YYYY-MM-DD HH:mm:ssZZ').substring(0, 22);
+    price = Math.max(Math.round(100 * price + trend
+      * faker.random.number({ min: 0, max: 1, precision: 0.01 })) / 100, 0.05);
+    rowsStr += `'${symbol}', '${ts}', ${price}\n`;
+
+    time.add(5, 'minutes');
+    if (faker.random.number(100) > 97) {
+      trend = faker.random.number({ min: -1, max: 1, precision: 0.01 });
+    }
+  }
+  return [null, rowsStr];
 };
 
 const genStocks = (qty) => {
@@ -24,76 +80,6 @@ const genStocks = (qty) => {
     stocks.push([symbol, name, owners, analystHold]);
   }
   return stocks;
-};
-
-const genStocksCSV = async (qty) => {
-  const filename = 'stocks.csv';
-  const csvFile = path.resolve(__dirname, 'seedFiles', filename);
-  const encoding = 'utf-8';
-  const writer = fs.createWriteStream(csvFile);
-  let i = qty;
-
-  const write = (cb) => {
-    const symbols = [];
-    let canWrite = true;
-    do {
-      i -= 1;
-      const symbol = faker.random.alphaNumeric(5).toUpperCase();
-      symbols.push(symbols);
-      const name = faker.company.companyName().replace(/'|,/g, '');
-      const owners = faker.random.number(20000);
-      const analystHold = faker.random.number({ min: 0, max: 100, precision: 1 });
-      const row = `'${symbol}','${name}',${owners},${analystHold}\n`;
-
-      if (i % 50000 === 0) {
-        fancy(`${qty - i} rows written for ${filename}`);
-      }
-
-      if (i === 0) {
-        writer.write(row, encoding, () => cb([csvFile, symbols]));
-      } else {
-        canWrite = writer.write(row, encoding);
-      }
-    } while (i > 0 && canWrite);
-
-    if (i > 0) {
-      writer.once('drain', () => write(cb));
-    }
-  };
-
-  return new Promise((resolve) => write(resolve));
-};
-
-const genCSV = async (filename, genRow, rowCount) => {
-  const csvFile = path.resolve(__dirname, 'seedFiles', filename);
-  const encoding = 'utf-8';
-  const writer = fs.createWriteStream(csvFile);
-  let i = rowCount;
-
-  const write = (cb, rowIds = []) => {
-    let canWrite = true;
-    do {
-      i -= 1;
-      const rowData = genRow();
-      rowIds.push(rowData[0]);
-
-      if (i % 50000 === 0) {
-        fancy(`${rowCount - i}/${rowCount} rows written for ${filename}`);
-      }
-
-      if (i === 0) {
-        writer.write(rowData[1], encoding, () => cb(rowIds));
-      } else {
-        canWrite = writer.write(rowData[1], encoding);
-      }
-    } while (i > 0 && canWrite);
-
-    if (i > 0) {
-      writer.once('drain', () => write(cb, rowIds));
-    }
-  };
-
-  return new Promise((resolve) => write(resolve));
 };
 
 const genTags = () => {
@@ -156,10 +142,10 @@ const genPriceHistory = () => {
 };
 
 module.exports = {
-  genStockRow,
-  genStocks,
-  genStocksCSV,
   genCSV,
+  genStockRow,
+  genPriceHistoryRows,
+  genStocks,
   genTags,
   genUsers,
   genUserStocks,
