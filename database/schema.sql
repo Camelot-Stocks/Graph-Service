7ae5af4bf203ba8d8e1c59ff61a929bac7ee2234
nickzylstra <nick.zylstra@gmail.com>
2019-12-29 19:41:20 -0800

    Setup db perf test suite

--- database/schema.sql
+++ database/schema.sql
@@ -24,7 +24,26 @@ CREATE TABLE IF NOT EXISTS prices (
   -- break out timestamp into subcomponents for faster querying?
   price NUMERIC(10,2)
 );
-CREATE INDEX ON prices (stock_symbol);
+-- CREATE INDEX ON prices (stock_symbol);
+
+CREATE OR REPLACE FUNCTION extract_min(ts timestamptz)
+RETURNS double precision AS
+$$select extract(minute from ts);$$
+LANGUAGE sql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION extract_hour(ts timestamptz)
+RETURNS double precision AS
+$$select extract(hour from ts);$$
+LANGUAGE sql IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION extract_dow(ts timestamptz)
+RETURNS double precision AS
+$$select extract(dow from ts);$$
+LANGUAGE sql IMMUTABLE;
+-- this one is not actually immutable and would cause issues in other countries with
+-- alternate day of week numbering schema
+
+CREATE INDEX prices_search_idx ON prices (stock_symbol, ts, extract_min(ts), extract_hour(ts), extract_dow(ts));
 
 CREATE TABLE IF NOT EXISTS users (
   user_id SERIAL PRIMARY KEY,
