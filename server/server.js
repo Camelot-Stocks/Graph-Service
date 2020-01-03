47c48b3e34c787e47d118793dac8199d6a752dfe
nickzylstra <nick.zylstra@gmail.com>
2020-01-02 20:09:36 -0800

    Switch ports for proxy testing

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ require('newrelic');
 const fancy = require('fancy-log');
 const app = require('./app');
 
-const port = 3000;
+const port = 3001;
 
 app.listen(port, () => { fancy.info(`server running on ${port}`); });
